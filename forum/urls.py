39052ad31dde11ef36f33a4e558c77034bf41ccd
ItsJustPedr0 <peterestacio09@gmail.com>
2026-05-02 22:48:57 +0800

    MP3 | Add post functionality

--- forum/urls.py
+++ forum/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.index, name="index"),
+  path("", views.index, name="index"),  
+  path('get-profs/', views.get_profs_by_dept, name='get_profs'),
+  path('get-courses/', views.get_courses_by_prof, name='get_courses'),
+  path('get-courses-by-dept/', views.get_courses_by_dept, name='get_courses_by_dept'),
+  path('add-post/', views.add_post, name="add_post"),
   path('search/', views.search, name='search'),
   path("<str:abbrev>/", views.dept_index, name="dept_index"),
   path("<str:abbrev>/course/<str:course_slug>/", views.course_detail, name="course_detail"),
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.template import loader
 from django.http import Http404
 from django.http import HttpResponse
@@ -80,5 +80,83 @@ def search(request):
         'dept': prof.dept.name if prof.dept else '',
         'url': f'/forum/{prof.dept.slug}/prof/{prof.slug}/'
       })
-
   return JsonResponse({'results': results, 'query': query})
+
+def add_post(request):
+  depts = Department.objects.all()
+  profs = Prof.objects.none()
+  courses = Course.objects.none()
+
+  if request.method == 'POST':
+    title = request.POST.get('title')
+    body = request.POST.get('body')
+    prof_id = request.POST.get('prof_id')
+    course_id = request.POST.get('course_id')
+
+    print("prof_id:", prof_id)
+    print("course_id:", course_id)
+    print("POST data:", request.POST)
+
+    # handle prof
+    if prof_id == 'NEW':
+      first_name = request.POST.get('new_prof_first_name', '').strip()
+      last_name = request.POST.get('new_prof_last_name', '').strip()
+      if not first_name or not last_name:
+          return HttpResponse("Please enter both first and last name.", status=400)
+      dept_id = request.POST.get('dept_id')
+      dept = get_object_or_404(Department, id=dept_id)
+      prof = Prof.objects.create(
+        first_name=first_name,
+        last_name=last_name,
+        dept=dept
+      )
+    else:
+      prof = get_object_or_404(Prof, id=prof_id)
+
+    # handle course
+    if course_id == 'NEW':
+      new_course_name = request.POST.get('new_course_name', '').strip()
+      if not new_course_name:
+        return HttpResponse("Please enter a course name.", status=400)
+      
+      dept = prof.dept
+      if dept is None:
+        return HttpResponse("Selected professor has no department assigned.", status=400)
+
+      if ':' in new_course_name:
+        code, name = new_course_name.split(':', 1)
+        code = code.strip()
+        name = name.strip()
+      else:
+        code = new_course_name
+        name = new_course_name
+      
+      course = Course.objects.create(code=code, name=name, dept=dept)
+      course.prof.add(prof)
+    else:
+      course = get_object_or_404(Course, id=course_id)
+
+    Post.objects.create(title=title, body=body, prof=prof, course=course)
+    return redirect('course_detail', abbrev=course.dept.slug, course_slug=course.slug)
+
+  template = loader.get_template("forum/add_post.html")
+  context = {"depts": depts, "profs": profs, "courses": courses}
+  return HttpResponse(template.render(context, request))
+
+def get_profs_by_dept(request):
+  dept_id = request.GET.get('dept_id')
+  dept = get_object_or_404(Department, id=dept_id)
+  profs = Prof.objects.filter(dept=dept).values('id', 'first_name', 'last_name', 'slug')
+  return JsonResponse({'profs': list(profs)})
+
+def get_courses_by_prof(request):
+  prof_id = request.GET.get('prof_id')
+  prof = get_object_or_404(Prof, id=prof_id)
+  courses = Course.objects.filter(prof=prof).values('id', 'name', 'code')
+  return JsonResponse({'courses': list(courses)})
+
+def get_courses_by_dept(request):
+    dept_id = request.GET.get('dept_id')
+    dept = get_object_or_404(Department, id=dept_id)
+    courses = Course.objects.filter(dept=dept).values('id', 'name', 'code')
+    return JsonResponse({'courses': list(courses)})
