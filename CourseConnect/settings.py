201c79a020e51c75c4a4ab5251973aad96d78509
ItsJustPedr0 <peterestacio09@gmail.com>
2026-05-05 05:51:33 +0800

    MP3 | Deployment hosts set

--- CourseConnect/settings.py
+++ CourseConnect/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY",default="django-insecure-p=+3lpwl$*!ylw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://courseconnect-etur.onrender.com"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
